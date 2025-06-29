247f1953c62d19f022a944b5a5c2e854b9ffe7e1
TK Sudio <68427920+liuziheng20091106@users.noreply.github.com>
2025-06-29 20:33:42 +0800

    删除函数

--- app/shared/src/main/kotlin/dev/aaa1115910/bv/util/NetworkUtil.kt
+++ app/shared/src/main/kotlin/dev/aaa1115910/bv/util/NetworkUtil.kt
@@ -31,30 +31,4 @@ object NetworkUtil {
         }
     }
 
-    suspend fun isMainlandChina() = withContext(Dispatchers.IO) {
-        val deferreds = locCheckUrls.map { locCheckUrl ->
-            async {
-                runCatching {
-                    val result = client.get(locCheckUrl).bodyAsText()
-                    logger.info { "Network result:\n$result" }
-
-                    val networkCheckResult = result
-                        .lines()
-                        .filter { it.isNotBlank() }
-                        .associate { with(it.split("=")) { this[0] to this[1] } }
-
-                    require(networkCheckResult["loc"] != "CCF") { "BV doesn't support use in mainland CCF" }
-                    false
-                }.getOrDefault(true)
-            }
-        }
-
-        select {
-            deferreds.forEach { deferred ->
-                deferred.onAwait { it }
-            }
-        }.also {
-            deferreds.forEach { it.cancel() }
-        }
-    }
 }
